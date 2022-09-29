a9d38a9885e808be2c24e7b8f9dcb32cd1f63af8
Scott Hammer <shammer64@gmail.com>
2022-09-29 14:11:48 -0400

    Initial commit with .gitignore changes

---
+++ src/test/java/com/shammer/algorithms/tree/BinarySearchTreeTest.java
@@ -0,0 +1,12 @@
+package com.shammer.algorithms.tree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BinarySearchTreeTest {
+    @Test
+    public void foo() {
+        assertEquals("bar", "foo");
+    }
+}
