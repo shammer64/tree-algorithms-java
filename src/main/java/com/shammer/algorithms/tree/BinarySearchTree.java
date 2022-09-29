7b93f047c02e675f1bd6418fd301ede4f02794dc
Scott Hammer <shammer64@gmail.com>
2022-09-29 16:05:21 -0400

    SH Introduced cardinality and depth of empty bst

---
+++ src/main/java/com/shammer/algorithms/tree/BinarySearchTree.java
@@ -0,0 +1,11 @@
+package com.shammer.algorithms.tree;
+
+public class BinarySearchTree<T> {
+    public long cardinality() {
+        return 0;
+    }
+
+    public long depth() {
+        return 0;
+    }
+}
--- src/test/java/com/shammer/algorithms/tree/BinarySearchTreeTest.java
+++ src/test/java/com/shammer/algorithms/tree/BinarySearchTreeTest.java
@@ -9,4 +9,26 @@ public class BinarySearchTreeTest {
     public void foo() {
         assertEquals("bar", "foo");
     }
+
+    // Tree Operations: insert, delete, search, traverse, cardinality, depth
+    // Empty tree has cardinality 0, depth 0, search for any item returns null
+    // Tree with one item has cardinality 1, depth 1, search for item returns item
+    // Tree with two items has cardinality 2, depth 2, search for item returns item
+    // Tree with three items (balanced) has cardinality 3, depth 2
+    // Tree with three items (unbalanced) has cardinality 3, depth 3
+
+    @Test
+    public void emptyTreeHasCardinalityOfZero() {
+        BinarySearchTree<String> bst = new BinarySearchTree();
+
+        assertEquals(0, bst.cardinality());
+    }
+
+    @Test
+    public void emptyTreeHasDepthOfZero() {
+        BinarySearchTree<String> bst = new BinarySearchTree();
+
+        assertEquals(0, bst.depth());
+    }
+
 }
