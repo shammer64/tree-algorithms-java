288225e08945d308a5f9a96549148e7a7e966771
cm42git <106208621+cm42git@users.noreply.github.com>
2022-11-01 19:45:38 -0400

    CM Created value and insert methods, added test for empty tree

--- src/main/java/com/shammer/algorithms/tree/AVLSearchTree.java
+++ src/main/java/com/shammer/algorithms/tree/AVLSearchTree.java
@@ -1,11 +1,21 @@
 package com.shammer.algorithms.tree;
 
 public class AVLSearchTree<T> {
+    private T value;
+
     public long cardinality() {
-        return 0;
+        return this.value == null ? 0 : 1;
     }
 
     public long depth() {
         return 0;
+    }
+
+    public T value() {
+        return this.value;
+    }
+
+    public void insert(T value) {
+        this.value=value;
     }
 }
--- src/test/java/com/shammer/algorithms/tree/AVLTreeTest.java
+++ src/test/java/com/shammer/algorithms/tree/AVLTreeTest.java
@@ -2,7 +2,9 @@ package com.shammer.algorithms.tree;
 
 import org.junit.jupiter.api.Test;
 
+import static com.shammer.algorithms.tree.BigTenSchools.IOWA;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class AVLTreeTest {
 
@@ -16,6 +18,21 @@ public class AVLTreeTest {
     public void emptyTreeHasDepthOfZero() {
         AVLSearchTree<String> ast = new AVLSearchTree<>();
         assertEquals(0, ast.depth());
+    }
+
+    @Test
+    public void emptyTreeHasNullValueAndHasCardinalityOfZero() {
+        AVLSearchTree<String> ast = new AVLSearchTree<>();
+        assertEquals(0, ast.cardinality());
+        assertNull(ast.value());
+    }
+
+    @Test
+    public void emptyTreeCanBeInsertedIntoAndHasCardinalityOfOne() {
+        AVLSearchTree<String> ast = new AVLSearchTree<>();
+        ast.insert(IOWA);
+        assertEquals(1, ast.cardinality());
+        assertEquals(IOWA, ast.value());
     }
 
 }
